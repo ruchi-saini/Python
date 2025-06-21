612b4431545419c0372812a051b602679d112f71
hp <hp@DESKTOP-5FIN00R>
2025-06-21 17:18:39 +0530

    Add project files.

---
+++ Python1/Python1.py
@@ -0,0 +1,28 @@
+
+# (1) WAP to take input from user
+
+"""input1=int(input("enter a first number:"))
+input2=int(input("enter a second number:"))
+sum=input1 + input2
+print(sum)"""
+
+# (2) List [Description: List is mutable and we can use slicing in List]
+
+# Wap to check the list is Palindrome or not
+
+"""l1=[1,2,1]
+listt= l1.copy()
+listt.reverse()
+if(l1==listt):
+  print("Palindrome")
+else:
+   print("Not Palindrome")"""
+
+
+# (3) Tuple (immutable)
+  
+t1=(1,2,3)
+print(t1[0:2])
+
+
+
